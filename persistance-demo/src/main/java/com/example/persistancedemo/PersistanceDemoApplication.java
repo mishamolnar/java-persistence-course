296178a3ab2674a7d882bd050ca4366c5ead53fc
mmolnar <mmolnar@atlassian.com>
2022-07-22 10:14:52 +0300

    initial commit

---
+++ persistance-demo/src/main/java/com/example/persistancedemo/PersistanceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.persistancedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistanceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistanceDemoApplication.class, args);
+	}
+
+}
---
+++ persistance-demo/src/test/java/com/example/persistancedemo/PersistanceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.persistancedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistanceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
